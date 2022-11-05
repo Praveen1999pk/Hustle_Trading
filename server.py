d0cd01eeba33da8e31785d8439425d808331f5d3
Praveen1999pk <104712152+Praveen1999pk@users.noreply.github.com>
2022-11-05 09:22:15 -0400

    first button

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, url_for
 
 app = Flask(__name__)
 
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("CreateAlgo.html")
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
-
