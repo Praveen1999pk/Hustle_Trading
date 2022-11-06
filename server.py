7466c620c76289c96b0cdf83d1900735e2ae619c
yogesh <39838149+yogesh-m-2@users.noreply.github.com>
2022-11-06 06:57:54 -0500

    database

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import mysql.connector
+import sqlite3
 
 app = Flask(__name__)
 
@@ -7,5 +9,10 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 if __name__=='__main__':
+    #mydb=mysql.connector.connect(host="localhost",user="",passwd="")
+    mydb=sqlite3.connect("database1.db")
+    query=mydb.cursor()
+    res=query.execute("SELECT * FROM bucket")
+    print(res.fetchall())
     app.run(debug=True)
 
